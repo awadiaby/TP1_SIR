5c6858ecaa9df53b061b2f2e42046220054f1eea
awadiaby <awa.diaby@etudiant.univ-rennes1.fr>
2021-01-27 15:45:20 +0100

    Initialisation un projet maven

---
+++ src/main/java/com/itextpdf/text/Anchor.java
@@ -0,0 +1,20 @@
+package com.itextpdf.text;
+
+public class Anchor {
+
+	public Anchor(String string, Font catFont) {
+		// TODO Auto-generated constructor stub
+	}
+
+	
+
+
+
+	public void setName(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+}
